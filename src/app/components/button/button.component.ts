39d8591e732270d4a97c6b978e17befb886da333
christian figueiredo <developer.figueiredo@gmail.com>
2025-09-25 11:43:52 -0300

    add button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: false,
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+ @Input() textButton: string | undefined;
+}
